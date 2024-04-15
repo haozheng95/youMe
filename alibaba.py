36cadb0c63f0783360dcddfb98cca3e6cb86bb40
haozheng95 <77625961@qq.com>
2024-04-15 15:08:35 +0800

    add verify code

--- alibaba.py
+++ alibaba.py
@@ -25,8 +25,8 @@ class Sample:
         @throws Exception
         """
         config = open_api_models.Config(
-            access_key_id="",
-            access_key_secret=""
+            access_key_id="LTAI5tQVPHrHgMqLojBuq5JY",
+            access_key_secret="HtDWd6KCZB3nhefpMkZIBFn5YHQ01h"
         )
         config.endpoint = f'dysmsapi.aliyuncs.com'
         return OpenApiClient(config)
